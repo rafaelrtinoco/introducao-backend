5bcdcb0d85176662da39a0852461f9b9a881554b
Rafael Ribeiro <59180005+rafaelrtinoco@users.noreply.github.com>
2024-10-05 14:09:19 +0000

    primeios arquivos php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá Mundo</title>
+</head>
+<body>
+
+    <?php
+        $primeiroNumero = 25;
+        $segundoNumero = 25;
+
+        $soma = $primeiroNumero + $segundoNumero;
+    
+    ?>
+
+    <p>A soma dos numeros <?php $primeiroNumero?> e <?php $segundoNumero?> é: <?php echo $soma ?> </p>
+    
+</body>
+</html>
